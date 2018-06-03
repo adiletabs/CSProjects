aa0ff64ce37deb818a57dd6deef25b554afc7788
Adilet Absatov <adl.absatov@gmail.com>
2018-06-04 01:51:10 +0600

    A classical game

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
+            this.Start = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Label();
+            this.Display2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.White;
+            this.Start.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start.Location = new System.Drawing.Point(0, 0);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(150, 45);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "New Game";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Display
+            // 
+            this.Display.AutoSize = true;
+            this.Display.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Display.Location = new System.Drawing.Point(193, 12);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(121, 21);
+            this.Display.TabIndex = 2;
+            this.Display.Text = "← Click to Start";
+            // 
+            // Display2
+            // 
+            this.Display2.AutoSize = true;
+            this.Display2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Display2.Location = new System.Drawing.Point(245, 12);
+            this.Display2.Name = "Display2";
+            this.Display2.Size = new System.Drawing.Size(0, 21);
+            this.Display2.TabIndex = 3;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(360, 405);
+            this.Controls.Add(this.Display2);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TicTacToe_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label Display;
+        private System.Windows.Forms.Label Display2;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        Graphics g;
+
+        Button[,] cells = new Button[3, 3];
+        int[,] whos = new int[3, 3];
+        bool end;
+
+        int size = 120;
+        int turn;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button btn = new Button();
+                    btn.Name = i.ToString() + j.ToString();
+                    btn.Size = new Size(size, size);
+                    btn.Location = new Point(i * size, j * size + 45);
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.BackColor = Color.WhiteSmoke;
+                    btn.Enabled = false;
+                    cells[i, j] = btn;
+                    Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+
+        private void TicTacToe_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawRectangle(new Pen(Color.Black, 3), 150, 0, 210, 45);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    cells[i, j].Enabled = true;
+                    cells[i, j].Image = null;
+                    cells[i, j].BackColor = Color.White;
+                    cells[i, j].Click += Process;
+                    whos[i, j] = -1;
+                }
+            }
+            turn = 0;
+            end = false;
+            Display.Text = "";
+            Display2.Text = "🗙";
+        }
+
+        private void Process(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int x = int.Parse(btn.Name[0].ToString());
+            int y = int.Parse(btn.Name[1].ToString());
+
+            if (btn.Image == null)
+            {
+                if (turn % 2 == 0)
+                {
+                    btn.Image = Properties.Resources.cross;
+                    whos[x, y] = 1;
+                }
+                else
+                {
+                    btn.Image = Properties.Resources.circle;
+                    whos[x, y] = 0;
+                }
+
+                turn++;
+                Check();
+
+                if (!end)
+                {
+                    if (turn % 2 == 1)
+                    {
+                        Display2.Text = "◯";
+                    }
+                    else
+                    {
+                        Display2.Text = "🗙";
+                    }
+                }
+            }
+        }
+
+        private void Check()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (whos[i, 0] >=0 && whos[i, 0] == whos[i, 1] && whos[i, 1] == whos[i, 2])
+                {
+                    cells[i, 0].Image = Properties.Resources.win;
+                    cells[i, 1].Image = Properties.Resources.win;
+                    cells[i, 2].Image = Properties.Resources.win;
+
+                    GameOver();
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (whos[0, i] >= 0 && whos[0, i] == whos[1, i] && whos[1, i] == whos[2, i])
+                {
+                    cells[0, i].Image = Properties.Resources.win;
+                    cells[1, i].Image = Properties.Resources.win;
+                    cells[2, i].Image = Properties.Resources.win;
+
+                    GameOver();
+                }
+            }
+
+            if (whos[0, 0] >= 0 && whos[0, 0] == whos[1, 1] && whos[1, 1] == whos[2, 2])
+            {
+                cells[0, 0].Image = Properties.Resources.win;
+                cells[1, 1].Image = Properties.Resources.win;
+                cells[2, 2].Image = Properties.Resources.win;
+
+                GameOver();
+            }
+
+            else if  (whos[0, 2] >= 0 && whos[0, 2] == whos[1, 1] && whos[1, 1] == whos[2, 0])
+            {
+                cells[0, 2].Image = Properties.Resources.win;
+                cells[1, 1].Image = Properties.Resources.win;
+                cells[2, 0].Image = Properties.Resources.win;
+
+                GameOver();
+            }
+
+            if (!end && turn == 9)
+            {
+                Display.Text = "         Draw";
+                Display2.Text = "";
+                end = true;
+            }
+
+            if (end)
+            {
+                foreach (Button btn in cells)
+                {
+                    btn.Click -= Process;
+                }
+            }
+        }
+
+        private void GameOver()
+        {
+            if (turn % 2 == 1)
+            {
+                Display.Text = "       🗙 wins!";
+                Display2.Text = "";
+            }
+            else if (turn % 2 == 0)
+            {
+                Display.Text = "       ◯ wins!";
+                Display2.Text = "";
+            }
+            end = true;
+        }
+    }
+}
